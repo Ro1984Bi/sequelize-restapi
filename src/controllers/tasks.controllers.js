7ff91d148ec0a2e3513ee1d9c86c69882cdaf5c7
Rodrigo Bilbao <bilbaorodrigojavi@outlook.com>
2022-09-30 14:56:02 -0400

    ready

---
+++ src/controllers/tasks.controllers.js
@@ -0,0 +1,72 @@
+import {Task} from '../models/Task.js'
+
+export const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.findAll()
+        res.json(tasks)
+    } catch (error) {
+       return res.status(500).json({message: error.message}) 
+    }
+}
+
+export const createTask = async (req, res) => {
+    try {
+        const { name, done, projectId } = req.body
+
+        const newTask = await Task.create({
+            name,
+            done,
+            projectId
+        })
+    
+        res.json(newTask)
+    } catch (error) {
+        return res.status(500).json({message: error.message}) 
+    }
+}
+
+export const getTask = async (req, res) => {
+    const { id } = req.params
+    try {
+        const task = await Task.findOne({
+            where: {
+                id
+            }
+        })
+        res.json(task)
+    } catch (error) {
+        return res.status(500).json({message: error.message}) 
+    }
+}
+
+export const updateTask = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const taskfound = await Task.findOne({
+            where: {
+                id
+            }
+        }) 
+        taskfound.set(req.body)
+        await taskfound.save()
+        return res.json(taskfound)
+    } catch (error) {
+        return res.status(500).json({message: error.message})  
+    }
+}
+
+export const deleteTask = async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedTask = await Task.destroy({
+            where: {
+                id
+            }
+        })
+        return res.sendStatus(204)
+        
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
